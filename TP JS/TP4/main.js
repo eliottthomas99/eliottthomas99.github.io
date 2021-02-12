362c8dd30b92a652a2098d2b4cc4ef2f4d59d5c0
Eliott <ethomas@enssat.fr>
2021-02-12 10:18:52 +0100

    2.7
    codé en dur la classe

--- TP JS/TP4/main.js
+++ TP JS/TP4/main.js
@@ -9,6 +9,28 @@ if (window.Worker) {
     initSquareIntervId = setInterval(newCoor, 500);
     canvas = document.getElementById('myCanvas');
 
+    class Point {
+        constructor(x, y, canvas) {
+            this.canvas = canvas;
+            this.canvas2dContext = this.canvas.getContext("2d");
+            this.x = x;
+            this.y = y;
+    
+            this.ache = function () {
+    
+                this.canvas2dContext.beginPath();
+                this.canvas2dContext.arc(this.x, this.y, 10, 0, 2 * Math.PI, false);
+                this.canvas2dContext.strokeStyle = 'black'; //l'extérieur
+                this.canvas2dContext.stroke();
+                this.canvas2dContext.fillStyle = 'cyan'; //l'intérieur
+                this.canvas2dContext.fill();
+    
+            };
+        }
+    }
+    
+
+
     function newCoor(){
         let num = Math.random();
         if(num<0.5){
