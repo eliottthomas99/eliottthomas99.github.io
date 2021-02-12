4f2d88f4e4e0d125098dd46c2e2273e88b5edf28
Eliott <ethomas@enssat.fr>
2021-02-12 11:46:30 +0100

    v 2.17
    modif des tailles

--- TP JS/TP4part2/worker1.js
+++ TP JS/TP4part2/worker1.js
@@ -5,7 +5,8 @@ onmessage = function(e) {
     coorx = Math.floor(Math.random()*600) ;
     coory = Math.floor(Math.random()*600); 
     console.log('Worker: Posting message back to main script');
-    postMessage([coorx,coory,"yellow"]);
+    taille = Math.floor(Math.random()*60);
+    postMessage([coorx,coory,"yellow",taille]);
    }
    else{
        postMessage('I don\'t get it')
--- TP JS/TP4part2/worker6.js
+++ TP JS/TP4part2/worker6.js
@@ -5,7 +5,9 @@ onmessage = function(e) {
     coorx = Math.floor(Math.random()*600) ;
     coory = Math.floor(Math.random()*600); 
     console.log('Worker: Posting message back to main script');
-    postMessage([coorx,coory,"purple"]);
+    taille = Math.floor(Math.random()*60);
+
+    postMessage([coorx,coory,"purple",taille]);
    }
    else{
        postMessage('I don\'t get it')
