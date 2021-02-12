2dfbf3e814c7e68c14016173719e10a31eb49c67
Eliott <ethomas@enssat.fr>
2021-02-12 11:33:26 +0100

    v 2.16
    chanegement de couleur

--- TP JS/TP4part2/worker9.js
+++ TP JS/TP4part2/worker9.js
@@ -5,7 +5,7 @@ onmessage = function(e) {
     coorx = Math.floor(Math.random()*600) ;
     coory = Math.floor(Math.random()*600); 
     console.log('Worker: Posting message back to main script');
-    postMessage([coorx,coory,"black"]);
+    postMessage([coorx,coory,"white"]);
    }
    else{
        postMessage('I don\'t get it')
